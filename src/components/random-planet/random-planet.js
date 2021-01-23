99086b49a2fa3c281371393943369541f2f77e37
landauleo <bambie7@mail.ru>
2021-01-23 14:31:06 +0300

    6.8

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,25 +1,48 @@
 import React, {Component} from "react";
 
 import './random-planet.css'
+import SwapiService from "../../services/swapi-service";
 
 //0 разметка компонентов
 export default class RandomPlanet extends Component {
+
+    swapiService = new SwapiService()
     //определение стэйта
     state = {
+        id: null,
         name: null,
         population: null,
         rotationPeriod: null,
         diameter: null
     };
 
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+
+    updatePlanet() {
+        const id = Math.floor(Math.random()*25 + 2);
+        this.swapiService.getPlanet(id)
+            .then((planet) => {
+                this.setState({
+                    id,
+                    name: planet.name,
+                    population: planet.population,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter
+                });
+            });
+    };
+
     render() {
 
-        const {name, population, rotationPeriod, diameter} = this.state;
+        const {id, name, population, rotationPeriod, diameter} = this.state;
 
         return (
             <div className="p=random-planet jumbotron rounded">
                 <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/5.jpg`}/>
+                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}/>
 
                 <div className="card-body">
                     <h4>{name}</h4>
